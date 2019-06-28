630aaf32d7595df112d1f07f62bea6ff236f924f
github username <github email address>
2019-06-27 23:10:01 -0400

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,34 @@
+module Findable
+    def find_by_name
+        def self.find_by_name(name)
+            @@songs.detect{|a| a.name == name}
+          end
+          def self.find_by_name(name)
+            @@artists.detect{|a| a.name == name}
+          end
+    end
+
+
+end 
+
+
+
+
+# class Artist
+
+#     @@artists = []
+  
+#     attr_accessor :name
+  
+#     def initialize(name)
+#       @name = name
+#     end
+  
+#     def self.all
+#       @@artists
+#     end
+  
+#     def self.find_by_name(name)
+#       @@artists.detect {|a| a.name == name}
+#     end
+#   end
